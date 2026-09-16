4a706bd054917652ed5aa196680b4b34e4af4aab
kirby69421-pixel <kirby69421@gmail.com>
2026-09-15 20:18:43 -0400

    Update Firebase configuration with project details

--- firebase-config.js
+++ firebase-config.js
@@ -3,11 +3,12 @@
 export const firebaseEnabled = true;
 
 export const firebaseConfig = {
-  apiKey: "PASTE_API_KEY_HERE",
-  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://PASTE_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "PASTE_MESSAGING_SENDER_ID",
-  appId: "PASTE_APP_ID"
+  apiKey: "AIzaSyARr3MN2CKk4VM6Ck75mfD7-pyrJFRCFBw",
+  authDomain: "wizards-of-the-west.firebaseapp.com",
+  databaseURL: "https://wizards-of-the-west-default-rtdb.firebaseio.com",
+  projectId: "wizards-of-the-west",
+  storageBucket: "wizards-of-the-west.firebasestorage.app",
+  messagingSenderId: "1040509732205",
+  appId: "1:1040509732205:web:4b709edfb9940328998664",
+  measurementId: "G-5DD68NQMSH"
 };
