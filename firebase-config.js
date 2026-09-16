60d3f2719df9198b0ed211be6e5eb1a003c8bc80
kirby69421-pixel <kirby69421@gmail.com>
2026-09-15 20:05:52 -0400

    Update Firebase configuration and initialization
    Updated Firebase configuration with new project details and enabled Firebase.

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,22 @@
-// Leave enabled=false to use guest mode.
-// Set enabled=true and replace the values when you connect Firebase.
-export const firebaseEnabled = false;
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-export const firebaseConfig = {
-  apiKey: "PASTE_API_KEY_HERE",
-  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://PASTE_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "PASTE_MESSAGING_SENDER_ID",
-  appId: "PASTE_APP_ID"
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyARr3MN2CKk4VM6Ck75mfD7-pyrJFRCFBw",
+  authDomain: "wizards-of-the-west.firebaseapp.com",
+  databaseURL: "https://wizards-of-the-west-default-rtdb.firebaseio.com",
+  projectId: "wizards-of-the-west",
+  storageBucket: "wizards-of-the-west.firebasestorage.app",
+  messagingSenderId: "1040509732205",
+  appId: "1:1040509732205:web:4b709edfb9940328998664",
+  measurementId: "G-5DD68NQMSH"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
